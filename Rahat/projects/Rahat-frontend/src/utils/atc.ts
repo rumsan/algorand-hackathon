23636c4e76ead87fa3ab009291c33aa08eef220b
subhashish-rumsan <subhashishjungshah@gmail.com>
2024-06-07 20:38:53 +0545

    ready to deploy

--- Rahat/projects/Rahat-frontend/src/utils/atc.ts
+++ Rahat/projects/Rahat-frontend/src/utils/atc.ts
@@ -3,46 +3,46 @@ import { algodClient } from "./typedClient";
 import { useWallet } from "@txnlab/use-wallet";
 import { PeraWalletConnect } from "@perawallet/connect";
 
-let methodInstance = algosdk.ABIMethod.fromSignature('sendTokenToBeneficiary(address,uint64,uint64)void');
+let methodInstance = algosdk.ABIMethod.fromSignature("sendTokenToBeneficiary(address,uint64,uint64)void");
 let selector = methodInstance.getSelector();
-const suggestedParams = await algodClient.getTransactionParams().do()
+
 const atc = new algosdk.AtomicTransactionComposer();
 
-const peraWallet = new PeraWalletConnect({chainId: 416002});    
+const peraWallet = new PeraWalletConnect({ chainId: 416002 });
 
-export const atomicTxnComposer = async (signerAddress: string, beneficiaryAddresses: string[], amount: number, assetId: number, sender: any) => {
+export const atomicTxnComposer = async (
+  signerAddress: string,
+  beneficiaryAddresses: string[],
+  amount: number,
+  assetId: number,
+  sender: any
+) => {
+  const suggestedParams = await algodClient.getTransactionParams().do();
+  await peraWallet.disconnect();
+  await peraWallet.connect();
+  let formattedAtc;
 
-    await peraWallet.disconnect();
-    await peraWallet.connect();
-    let formattedAtc;
+  const boxKey = algosdk.bigIntToBytes(assetId, 8);
+  beneficiaryAddresses.map((benAddress) => {
+    const txn = algosdk.makeApplicationNoOpTxnFromObject({
+      from: signerAddress as string,
+      appIndex: Number(import.meta.env.VITE_APP_ID),
+      appArgs: [selector, algosdk.decodeAddress(benAddress).publicKey, algosdk.encodeUint64(amount), algosdk.encodeUint64(assetId)],
+      suggestedParams: { ...suggestedParams, fee: 1000 },
+      foreignAssets: [assetId],
+      accounts: [benAddress],
+      boxes: [
+        {
+          appIndex: 0,
+          name: boxKey,
+        },
+      ],
+    });
 
-    const boxKey = algosdk.bigIntToBytes(assetId, 8);
-    beneficiaryAddresses.map((benAddress) => {
-        const txn = algosdk.makeApplicationNoOpTxnFromObject({
-            from: signerAddress as string,
-            appIndex: Number(import.meta.env.VITE_APP_ID),
-            appArgs: [
-              selector,
-              algosdk.decodeAddress(benAddress).publicKey,
-              algosdk.encodeUint64(amount),
-              algosdk.encodeUint64(assetId),
-            ],
-            suggestedParams: {...suggestedParams, fee: 1000},
-            foreignAssets: [assetId],
-            accounts: [benAddress],
-            boxes: [
-              {
-                appIndex: 0,
-                name: boxKey,
-              },
-            ],
-          });
-        
-         formattedAtc = atc.addTransaction({ txn, signer: sender.signer })
-    })
+    formattedAtc = atc.addTransaction({ txn, signer: sender.signer });
+  });
 
-    const signedTxn = await peraWallet.signTransaction([atc.buildGroup()])
+  const signedTxn = await peraWallet.signTransaction([atc.buildGroup()]);
 
-    return signedTxn;
-    
-}
+  return signedTxn;
+};
